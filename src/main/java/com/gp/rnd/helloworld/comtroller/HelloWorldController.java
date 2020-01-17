49590db2476b3307c354c3b36a6d5def3ea39dbe
Pandeya <Gyanesh.Pandeya@McKesson.com>
2020-01-16 23:00:25 -0800

    added expose in dockerfile and custom greet api

--- src/main/java/com/gp/rnd/helloworld/comtroller/HelloWorldController.java
+++ src/main/java/com/gp/rnd/helloworld/comtroller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.gp.rnd.helloworld.comtroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloWorldController {
 	@GetMapping("/greet")
 	public String sayHello() {
 		return "Hello from Controller!";
+	}
+	
+	@GetMapping("/greet/{name}")
+	public String customGreeting(@PathVariable String name) {
+		return "Hello, "+name+"!";
 	}
 }
